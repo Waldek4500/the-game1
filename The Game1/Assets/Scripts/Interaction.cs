5c9cb5ee92bc353e468e4afc5c7acef1521b4a7a
Waldek4500 <zygmuntowiczwaldek@gmail.com>
2026-02-25 10:54:07 +0100

    dev

---
+++ The Game1/Assets/Scripts/Interaction.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Interaction : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        Debug.Log("Test");
+    }
+}
--- The Game1/Assets/Scripts/Player/PlayerController.cs
+++ The Game1/Assets/Scripts/Player/PlayerController.cs
@@ -7,11 +7,38 @@ public class PlayerController : MonoBehaviour
     [Header("Player Movement")]
     // Player speed
     [SerializeField] private float _movementSpeed;
+
     // Player jump force
     [SerializeField] private float _jumpForce;
 
+    // Player sprint speed
+    [SerializeField] private float _sprintSpeed;
+
+    // Is player sprinting
+    private bool _isSprinting;
+
+    // Is player facing right
+    private bool _isFacingRight;
+
+    // Is player on ground
+    private bool _isGrounded;
+
+    [Header("Others")]
+    // Ground layer for grounded detection
+    [SerializeField] private LayerMask _groundLayer;
+
+    // Layer of items with player can interact
+    [SerializeField] private LayerMask _interactionLayer;
+
     // RigidBody2D component
     private Rigidbody2D _rb;
+
+    // BoxCollider2D component
+    [SerializeField] private BoxCollider2D _boxCollider;
+
+    // Animator component
+    private Animator _animator;
+
     // Vector2 from input system
     private Vector2 _movement;
     #endregion
@@ -19,19 +46,37 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        // Get start facing
+        _isFacingRight = transform.localScale.x > 0;
+
+        // Get components
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
-        
+        if (_movement.x > 0 && !_isFacingRight)
+            Flip();
+        else if (_movement.x < 0 && _isFacingRight)
+            Flip();
+
+
+        _animator.SetFloat("MoveX", Mathf.Abs(_rb.linearVelocity.x));
+
+        if(_animator.GetBool("Grounded") != IsGrounded())
+            _animator.SetBool("Grounded", IsGrounded());
+
+        Debug.Log(Mouse.current.position.ReadValue());
     }
 
     private void FixedUpdate()
     {
-        _rb.linearVelocity = new Vector2(_movement.x * _movementSpeed, _rb.linearVelocityY);
+        if(_isSprinting)
+            _rb.linearVelocity = new Vector2(_movement.x * _movementSpeed * _sprintSpeed, _rb.linearVelocityY);
+        else
+            _rb.linearVelocity = new Vector2(_movement.x * _movementSpeed, _rb.linearVelocityY);
     }
-
 
     #region Player Movement
     public void Move(InputAction.CallbackContext context)
@@ -41,16 +86,71 @@ public class PlayerController : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && IsGrounded())
         {
             _rb.linearVelocity = new Vector2(_rb.linearVelocityX, _jumpForce);
         }
     }
 
+    public void Sprint(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            _isSprinting = true;
+            return;
+        }
+
+        _isSprinting = false;
+    }
+
+    public void Interact(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            // Check for item with player can interact
+            Collider2D area = Physics2D.OverlapBox(_boxCollider.bounds.center, _boxCollider.bounds.size*5, 0, _interactionLayer);
+
+            // Retrun if not found
+            if (!area)
+                return;
+
+            // Get GameObject to interact
+            GameObject item = area.gameObject;
+
+            // Execute action
+            if(item.TryGetComponent<IInteractable>(out IInteractable interaction))
+            {
+                interaction.Interact();
+            }
+        }
+    }
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+        if(context.performed && IsGrounded())
+        {
+            _animator.SetTrigger("Attack");
+        }
+    }
+
+    public void Flip()
+    {
+        _isFacingRight = !_isFacingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
     private bool IsGrounded()
     {
-        return Physics2D.BoxCast(transform.position, )
+        return Physics2D.BoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size, 0, -Vector2.up, 0.3f, _groundLayer);
     }
 
     #endregion
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireCube(_boxCollider.bounds.center, _boxCollider.bounds.size * 5);
+    //}
 }
